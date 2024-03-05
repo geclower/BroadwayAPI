31c56adfa95d3b8805882b39dacb3cb97a573ed9
Grace Clower <clower.grace@gmail.com>
2024-03-05 12:12:55 -0500

    expanded and corrected model info

--- models/Show.js
+++ models/Show.js
@@ -2,15 +2,24 @@ import mongoose from "mongoose";
 
 const ShowSchema = new mongoose.Schema({
   title: String,
-  runTime: String,
+  runTime: Number,
   theater: String,
   theaterAddress: String,
-  openDate: String,
-  closeDate: String,
+  directedBy: Array,
+  bookBy: Array,
+  musicBy: { 
+    type: Array,
+    require: false},
+  openDate: Date,
+  isClosing: Boolean,
+  closeDate: {
+    type: Date,
+    require: false},
   intermission: Boolean,
   isMusical: Boolean,
   buyTickets: String,
-  officalSite: String
+  officalSite: String,
+  posterImage: String
 });
 
 export default mongoose.model("shows", ShowSchema);
